ede6ed689b12cccc16faf91ba51f172442d073e5
nprindle <nprindle18@gmail.com>
2020-05-17 19:25:32 -0400

    Add githug

--- default.nix
+++ default.nix
@@ -8,9 +8,14 @@
 
   # packages
   butler = pkgs.callPackage ./pkgs/butler {};
+
   riscv-gnu-toolchain = pkgs.callPackage ./pkgs/riscv-gnu-toolchain {};
+
   xs = pkgs.callPackage ./pkgs/xs {
     ocamlPackages = pkgs.ocamlPackages_latest;
   };
+
+  githug = pkgs.callPackage ./pkgs/githug {};
+
 }
 
